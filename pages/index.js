73346a73b99f8796a43ba4ec8f12669da83a0b7a
Prasad Chavan <97186762+prasad-chavan1@users.noreply.github.com>
2023-10-02 19:02:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <div>
             <Head>
+<meta name="google-site-verification" content="VJs2G3ynvYQ1KG06ciOhdPdklC1gIUEkJ2KcplrnfWU" />
                 <title>Home | Prasad Chavan</title>
             </Head>
             <HomePage/>
