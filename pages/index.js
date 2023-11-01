d52be70a728cf486d1acbc5270fae67dd1d33576
Prashuu <97186762+prasad-chavan1@users.noreply.github.com>
2023-11-01 11:03:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ export default function Home() {
         <div>
             <Head>
 <meta name="google-site-verification" content="VJs2G3ynvYQ1KG06ciOhdPdklC1gIUEkJ2KcplrnfWU" />
-                <title>Home | Prasad Chavan</title>
+        <meta name="description" content="Prasad Chavan portfolio. I am web developer, data analyst and machine learning enthusiast. PrasadChavan, Prasadchavan, prasad chavan">
+                <title>Prasad Chavan</title>
             </Head>
             <HomePage/>
         </div>
