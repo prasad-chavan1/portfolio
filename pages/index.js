32943d005d8ef115a4703bdc870b65ae9ee91e7b
Prashuu <97186762+prasad-chavan1@users.noreply.github.com>
2024-04-14 12:28:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
         <div>
             <Head>
 <meta name="google-site-verification" content="VJs2G3ynvYQ1KG06ciOhdPdklC1gIUEkJ2KcplrnfWU" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4054073151944400"
+     crossorigin="anonymous"></script>
         <meta name="description" content="Prasad Chavan portfolio. I am web developer, data analyst and machine learning enthusiast. PrasadChavan, Prasadchavan, prasad chavan"/>
                 <title>Prasad Chavan</title>
             </Head>
