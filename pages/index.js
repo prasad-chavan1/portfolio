c5c82346d51847899eead35586d43a7fe5b5e17e
Prashuu <97186762+prasad-chavan1@users.noreply.github.com>
2024-04-16 08:50:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
 <meta name="google-site-verification" content="VJs2G3ynvYQ1KG06ciOhdPdklC1gIUEkJ2KcplrnfWU" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4054073151944400"
      crossorigin="anonymous"></script>
-        <meta name="description" content="Prasad Chavan portfolio. I am web developer, data analyst and machine learning enthusiast. PrasadChavan, Prasadchavan, prasad chavan"/>
+        <meta name="description" content="Unleashing Your Creativity in Web Development: Tips and Tricks for Building Engaging Websites By Prasad Chavan"/>
                 <title>Prasad Chavan- Web Developer</title>
             </Head>
             <HomePage/>
